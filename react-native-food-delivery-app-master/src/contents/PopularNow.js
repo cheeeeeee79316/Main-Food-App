e67349992c5d7aa7898a369d27a190c1178250f8
cmrl <113733178+cheeeeeee79316@users.noreply.github.com>
2023-03-02 19:07:49 +0800

    Add files via upload

---
+++ react-native-food-delivery-app-master/src/contents/PopularNow.js
@@ -0,0 +1,32 @@
+export const popularNow = [
+  {
+    id: 1,
+    title: 'Double Beef Burger',
+    subtitle: 'Extra yummy beef🥩',
+    price: 169.00,
+    image: require('../assets/images/double-beef-burger.png'),
+    bestSeller: true,
+  },
+  {
+    id: 2,
+    title: 'Sandwich Burger',
+    subtitle: 'Why not both?🤤',
+    price: 157.00,
+    image: require('../assets/images/sandwich-burger.png'),
+  },
+  {
+    id: 3,
+    title: 'Deluxe Beef Burger',
+    subtitle: 'Extra double beef🥩',
+    price: 189.00,
+    image: require('../assets/images/double-beef-burger.png'),
+    bestSeller: true,
+  },
+  {
+    id: 4,
+    title: 'Special Yummy HamBurger',
+    subtitle: 'Bacon? Why not?🤤',
+    price: 149.00,
+    image: require('../assets/images/sandwich-burger.png'),
+  },
+];
